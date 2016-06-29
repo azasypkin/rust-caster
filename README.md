9763dacd492783e3ed6bdc8257fb90a91c0b8f9e
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-06-30 00:14:10 +0200

    Refactor to use the latest chromecast-link lib + introduce command line arguments.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 [![Build Status](https://travis-ci.org/azasypkin/chromecast-link-tool.svg?branch=master)](https://travis-ci.org/azasypkin/chromecast-link-tool)
 
 Just a helper tool for Rust [Chromecast Link](https://github.com/azasypkin/chromecast-link) crate.
+
+## Usage
+```bash
+$ cargo run -- -a chromecast.local --play http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 --content-type video/mp4
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![feature(plugin)]
+#![plugin(docopt_macros)]
+
+extern crate docopt;
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+extern crate rustc_serialize;
 extern crate chromecast_link;
 
 use chromecast_link::Chromecast;
@@ -6,49 +14,68 @@ use chromecast_link::channels::receiver;
 
 const DEFAULT_DESTINATION_ID: &'static str = "receiver-0";
 
-fn main() {
-    let mut chromecast = Chromecast::new("az_chromecast.local".to_owned(), 8009);
+docopt!(Args derive Debug, "
+Usage: chromecast-link-tool [-v] [-h] [-a <address>] [-p <port>] [--play <url>] [--media-type <media type>]
 
-    if let Err(err) = chromecast.connect() {
-        panic!("Chromecast is unable to establish connection: {:?}", err);
+Options:
+    -a, --address <address>             Chromecast's network address.
+    -p, --port <port>                   Chromecast's network port. [default: 8009]
+        --play <url>                    Play specified URL on Chromecast connected device.
+        --media-type <media_type>       Media type of the video to play. [default: video/mp4]
+    -v, --verbose                       Toggle verbose output.
+    -h, --help                          Print this help menu.
+",
+        flag_address: Option<String>,
+        flag_port: u16,
+        flag_play: Option<String>,
+        flag_media_type: String
+);
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+
+    if args.flag_address.is_none() {
+        println!("Please specify Chromecast's address!");
+        std::process::exit(1);
     }
 
-    let heartbeat_channel = chromecast.create_heartbeat_channel();
-    let connection_channel = chromecast.create_connection_channel();
-    let receiver_channel = chromecast.create_receiver_channel();
+    let chromecast = match Chromecast::connect(args.flag_address.unwrap(), args.flag_port) {
+        Ok(chromecast) => chromecast,
+        Err(err) => panic!("Chromecast is unable to establish connection: {:?}", err)
+    };
 
     let mut media_loaded = false;
+    let media_url: String = args.flag_play.unwrap_or("".to_owned());
+    let media_type = args.flag_media_type;
 
-    connection_channel.connect(DEFAULT_DESTINATION_ID.to_owned());
-    heartbeat_channel.ping();
-    receiver_channel.launch_app(CHROMECAST_APPS.default_media_receiver.to_owned());
+    chromecast.connection.connect(DEFAULT_DESTINATION_ID.to_owned());
+    chromecast.heartbeat.ping();
+    chromecast.receiver.launch_app(CHROMECAST_APPS.default_media_receiver.to_owned());
 
     loop {
-        let message = chromecast.receive();
+        let message = chromecast.receive().unwrap();
 
-        if let Ok(payload) = heartbeat_channel.try_handle(&message) {
+        if let Ok(payload) = chromecast.heartbeat.try_handle(&message) {
             if payload.typ == "PING" {
-                heartbeat_channel.pong();
+                chromecast.heartbeat.pong();
             }
-        } else if let Ok(payload) = connection_channel.try_handle(&message) {
+        } else if let Ok(payload) = chromecast.connection.try_handle(&message) {
             println!("Connection channel message received: {:?}", payload);
-        } else if let Ok(payload) = receiver_channel.try_handle(&message) {
+        } else if let Ok(payload) = chromecast.receiver.try_handle(&message) {
             match payload {
                 receiver::Reply::Status(reply) => {
-                    if reply.status.applications.len() > 0 && !media_loaded {
+                    if reply.status.applications.len() > 0 && !media_loaded && !media_url.is_empty() {
                         let application = &reply.status.applications[0];
 
-                        // Connect to application.
-                        connection_channel.connect(application.transport_id.clone());
+                        chromecast.connection.connect(application.transport_id.clone());
 
-                        let media_channel =
-                            chromecast.create_media_channel(application.transport_id.clone(),
-                                                            application.session_id.clone());
+                        let media_channel = chromecast.create_media_channel(
+                            application.transport_id.clone(),
+                            application.session_id.clone()).unwrap();
 
-                        media_channel.load("http://commondatastorage.googleapis.\
-                                            com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                                               .to_owned(),
-                                           "video/mp4".to_owned());
+                        media_channel.load(media_url.as_ref(), media_type.as_ref());
 
                         media_loaded = true;
                     }
