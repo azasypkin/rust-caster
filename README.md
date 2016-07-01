4bb58d428e42fb4ae9d5e18b7f7adeac05e0f58e
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-07-01 18:58:24 +0200

    Add --stop, --run, --info commands + more docs.

--- README.md
+++ README.md
@@ -5,8 +5,39 @@
 Just a helper tool for Rust [Chromecast Link](https://github.com/azasypkin/chromecast-link) crate.
 
 ## Usage
+
+### Generic features
 ```bash
-$ cargo run -- -a chromecast.local --stream http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4 --media-type video/mp4
-$ cargo run -- -a chromecast.local --stream http://xxx.mp4 --stream-type buffered
-& cargo run -- -a chromecast.local --stream 7LcUOEP7Brc --media-app youtube
+// Get some info about the Chromecast-enabled device.
+$ cargo run -- -a chromecast.local -i
+
+Number of apps run: 1
+App#0: Default Media Receiver (CC1AD845)
+Volume level: 1
+Muted: false
+
+// Run specific app on the Chromecast.
+$ cargo run -- -a chromecast.local -r youtube
+
+// Stop currently active app.
+$ cargo run -- -a chromecast.local -s
+
+The following app has been stopped: Default Media Receiver (CC1AD845)
 ```
+
+### Media features
+```bash
+// Stream a video.
+$ cargo run -- -a chromecast.local -m http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4
+
+// Stream a video of specific format with buffering.
+$ cargo run -- -a chromecast.local -m http://xxx.webm --media-type video/webm --media-stream-type buffered
+
+// Stream video from YouTube.
+$ cargo run -- -a chromecast.local -m 7LcUOEP7Brc --media-app youtube
+
+// Display an image.
+$ cargo run -- -a chromecast.local -m https://azasypkin.github.io/style-my-image/images/mozilla.jpg
+```
+
+For all possible values of `--media-type` see [Supported Media for Google Cast](https://developers.google.com/cast/docs/media).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(plugin)]
 #![plugin(docopt_macros)]
 
+extern crate ansi_term;
 extern crate docopt;
 extern crate env_logger;
 #[macro_use]
@@ -10,6 +11,8 @@ extern crate chromecast_link;
 
 use std::str::FromStr;
 
+use ansi_term::Colour::{Green, Red};
+
 use chromecast_link::Chromecast;
 use chromecast_link::channels::receiver::{ChromecastApp, Reply};
 use chromecast_link::channels::media::StreamType;
@@ -17,24 +20,30 @@ use chromecast_link::channels::media::StreamType;
 const DEFAULT_DESTINATION_ID: &'static str = "receiver-0";
 
 docopt!(Args derive Debug, "
-Usage: chromecast-link-tool [-v] [-h] [-a <address>] [-p <port>] [--stream <url>] [--media-type <media type>] [--stream-type <stream type>] [--media-app <media app>]
+Usage: chromecast-link-tool [-v] [-h] [-a <address>] [-p <port>] [-r <app to run>] [-s] [-i] [-m <media handle>] [--media-type <media type>] [--video-stream-type <stream type>] [--media-app <media app>]
 
 Options:
-    -a, --address <address>             Chromecast's network address.
-    -p, --port <port>                   Chromecast's network port. [default: 8009]
-        --stream <url>                  Stream specified URL to Chromecast connected device.
-        --media-type <media_type>       Media type of the video to play. [default: video/mp4]
-        --stream-type <stream_type>     Stream type to use (buffered, live or unknown). [default: unknown]
-        --media-app <media_app>         Media app to use for streaming. [default: default]
-    -v, --verbose                       Toggle verbose output.
-    -h, --help                          Print this help menu.
+    -a, --address <address>                 Chromecast's network address.
+    -p, --port <port>                       Chromecast's network port. [default: 8009]
+    -r, --run <app_to_run>                  Run the app with specified id/name.
+    -s, --stop                              Stops currently active app.
+    -i, --info                              Returns the info about the receiver.
+    -m, --media <media_handle>              Media handle (URL for image or video, URL token for youtube video etc.) to load on the Chromecast connected device.
+        --media-type <media_type>           Type of the media to load.
+        --media-app <media_app>             Media app to use for streaming. [default: default]
+        --media-stream-type <stream_type>   Media stream type to use (buffered, live or none). [default: none]
+    -v, --verbose                           Toggle verbose output.
+    -h, --help                              Print this help menu.
 ",
         flag_address: Option<String>,
         flag_port: u16,
-        flag_stream: Option<String>,
-        flag_media_type: String,
-        flag_stream_type: String,
+        flag_run: Option<String>,
+        flag_stop: Option<String>,
+        flag_info: Option<String>,
+        flag_media: Option<String>,
+        flag_media_type: Option<String>,
         flag_media_app: String,
+        flag_media_stream_type: String,
 );
 
 fn main() {
@@ -52,48 +61,96 @@ fn main() {
         Err(err) => panic!("Chromecast is unable to establish connection: {:?}", err)
     };
 
-    let mut media_loaded = false;
-    let stream_url: String = args.flag_stream.unwrap_or("".to_owned());
-    let media_type = args.flag_media_type;
+    chromecast.connection.connect(DEFAULT_DESTINATION_ID.to_owned()).unwrap();
+    chromecast.heartbeat.ping().unwrap();
+    chromecast.receiver.get_status().unwrap();
 
-    chromecast.connection.connect(DEFAULT_DESTINATION_ID.to_owned());
-    chromecast.heartbeat.ping();
-    chromecast.receiver.launch_app(ChromecastApp::from_str(args.flag_media_app.as_ref()).unwrap());
+    let media = args.flag_media.unwrap_or("".to_owned());
+    let media_type = args.flag_media_type.unwrap_or("".to_owned());
 
     loop {
         let message = chromecast.receive().unwrap();
 
         if let Ok(payload) = chromecast.heartbeat.try_handle(&message) {
             if payload.typ == "PING" {
-                chromecast.heartbeat.pong();
+                chromecast.heartbeat.pong().unwrap();
             }
         } else if let Ok(payload) = chromecast.connection.try_handle(&message) {
-            println!("Connection channel message received: {:?}", payload);
+            debug!("Connection channel message received: {:?}", payload);
         } else if let Ok(payload) = chromecast.receiver.try_handle(&message) {
             match payload {
                 Reply::Status(reply) => {
-                    if reply.status.applications.len() > 0 && !media_loaded && !stream_url.is_empty() {
-                        let application = &reply.status.applications[0];
+                    let apps = reply.status.applications;
 
-                        chromecast.connection.connect(application.transport_id.clone());
+                    if args.flag_info.is_some() {
+                        println!("\n{} {}",
+                                 Green.paint("Number of apps run:"),
+                                 Red.paint(apps.len().to_string()));
+                        for i in 0..apps.len() {
+                            println!("{}{}{}{}{}{}{}",
+                                     Green.paint("App#"),
+                                     Green.paint(i.to_string()),
+                                     Green.paint(": "),
+                                     Red.paint(apps[i].display_name.as_ref()),
+                                     Red.paint(" ("),
+                                     Red.paint(apps[i].app_id.as_ref()),
+                                     Red.paint(")"));
+                        }
+                        println!("{} {}",
+                                 Green.paint("Volume level:"),
+                                 Red.paint(reply.status.volume.level.to_string()));
+                        println!("{} {}\n",
+                                 Green.paint("Muted:"),
+                                 Red.paint(reply.status.volume.muted.to_string()));
+                        break;
+                    } else if args.flag_run.is_some() {
+                        let app = ChromecastApp::from_str(args.flag_run.as_ref().unwrap()).unwrap();
+                        chromecast.receiver.launch_app(app).unwrap();
+                      break;
+                    } else if args.flag_stop.is_some() {
+                        if apps.len() == 0 {
+                            println!("{}", Red.paint("There is no app to stop!"));
+                        } else {
+                            chromecast.receiver.stop_app(apps[0].session_id.as_ref()).unwrap();
+                            println!("{}{}{}{}{}",
+                                     Green.paint("The following app has been stopped: "),
+                                     Red.paint(apps[0].display_name.as_ref()),
+                                     Red.paint(" ("),
+                                     Red.paint(apps[0].app_id.as_ref()),
+                                     Red.paint(")"));
+                        }
 
-                        let media_channel = chromecast.create_media_channel(
-                            application.transport_id.clone(),
-                            application.session_id.clone()).unwrap();
+                        break;
+                    } else if !media.is_empty() {
+                        // Check if required app is run.
+                        let media_app = ChromecastApp::from_str(
+                            args.flag_media_app.as_ref()).unwrap();
 
+                        let app = apps.iter().find(|ref app| {
+                            ChromecastApp::from_str(app.app_id.as_ref()).unwrap() == media_app
+                        });
 
-                        let stream_type = match args.flag_stream_type.as_ref() {
-                            "buffered" => StreamType::Buffered,
-                            "live" => StreamType::Live,
-                            "unknown" => StreamType::Unknown,
-                            _ => panic!(
-                                "Unsupported stream type {}! Valid values are: buffered, live or unknown. ",
-                                args.flag_stream_type)
-                        };
+                        match app {
+                            None => chromecast.receiver.launch_app(media_app).unwrap(),
+                            Some(app) => {
+                                chromecast.connection.connect(app.transport_id.as_ref()).unwrap();
 
-                        media_channel.stream(stream_url.as_ref(), media_type.as_ref(), stream_type);
+                                let media_channel = chromecast.create_media_channel(
+                                    app.transport_id.as_ref(),
+                                    app.session_id.as_ref()).unwrap();
 
-                        media_loaded = true;
+                                let media_stream_type = match args.flag_media_stream_type.as_ref() {
+                                    "buffered" => StreamType::Buffered,
+                                    "live" => StreamType::Live,
+                                    "none" => StreamType::None,
+                                    _ => panic!("Unsupported stream type {}!",
+                                                args.flag_media_stream_type)
+                                };
+
+                                media_channel.load(media.as_ref(), media_type.as_ref(),
+                                                   media_stream_type).unwrap();
+                            }
+                        }
                     }
                 }
                 _ => {
